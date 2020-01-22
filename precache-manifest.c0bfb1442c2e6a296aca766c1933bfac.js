64bba3e4e8474b65e1694d28a33f1c8e64a8e385
steszxo <techlizardsmells@gmail.com>
2020-01-22 12:04:58 +0100

    Updates

---
+++ precache-manifest.c0bfb1442c2e6a296aca766c1933bfac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "928bfd44779182f35db8a11eb3db6300",
+    "url": "/steszxo/applicazione-1.git/index.html"
+  },
+  {
+    "revision": "01d091df71e75a7344cb",
+    "url": "/steszxo/applicazione-1.git/static/css/main.1359096b.chunk.css"
+  },
+  {
+    "revision": "5debf14d8864ec3f7c42",
+    "url": "/steszxo/applicazione-1.git/static/js/2.78f1477f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/steszxo/applicazione-1.git/static/js/2.78f1477f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01d091df71e75a7344cb",
+    "url": "/steszxo/applicazione-1.git/static/js/main.d3b1415b.chunk.js"
+  },
+  {
+    "revision": "a746b46adc5adcf9ee5e",
+    "url": "/steszxo/applicazione-1.git/static/js/runtime-main.547eb8a2.js"
+  }
+]);
